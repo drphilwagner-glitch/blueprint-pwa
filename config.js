44314cf583d61f1d3ea504f55b253d541d38da4b
drphilwagner-glitch <drphilwagner@gmail.com>
2026-08-03 06:48:49 -0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Deployed Apps Script Web App exec URL. Filled in at deploy time (S8 enablement / rollout).
 // Until set, the app shows a clear "not configured" message instead of failing silently.
 window.BP_CONFIG = {
-  WEBAPP_URL: 'https://script.google.com/macros/s/AKfycbxDvoEfElCnIfBAkzZ1iaqvkQcuRqAgoMQXkm-8OlU7Xs6MfjX9_UzW9-RrWJq1Q5E0/exec'
+  WEBAPP_URL: 'https://script.google.com/macros/s/AKfycbxunaq6uMY3iFZ0ky3iArBHM2UZrwyY9JzD-dipFMsAbl_OAtWwTa_l4GO_WAax__T0Vg/exec'
 };
